3171f9294fd98943b054dd799c2089c3b35652bf
Manuel Bouyer <bouyer@sandettie.soc.lip6.fr>
2020-02-15 09:48:51 +0100

    Factor out common code for transient objects in transient_open(). For symetry rename enc_group_close() to transcient_close().

--- software/edisplay/host/lv_edisplay/edisplay.c
+++ software/edisplay/host/lv_edisplay/edisplay.c
@@ -345,7 +345,15 @@ enc_group_defocus(lv_obj_t *obj)
 }
 
 static void
-enc_group_close(lv_obj_t *obj)
+transient_open(lv_obj_t *obj)
+{
+	lv_obj_move_foreground(obj);
+	lv_obj_align(obj, lv_top_trs, LV_ALIGN_CENTER, 0, 0);
+	enc_group_focus(obj, true);
+}
+
+static void
+transient_close(lv_obj_t *obj)
 {
 	enc_group_defocus(obj);
 	lv_obj_del_async(obj);
@@ -357,7 +365,7 @@ light_action(lv_obj_t *slide, lv_event_t event)
 	int key;
 	switch(event) {
 	case LV_EVENT_SHORT_CLICKED:
-		enc_group_close(slide);
+		transient_close(slide);
 		break;
 	case LV_EVENT_VALUE_CHANGED:
 		backlight_pwm = lv_slider_get_value(slide);
@@ -368,7 +376,7 @@ light_action(lv_obj_t *slide, lv_event_t event)
 		if (key == LV_KEY_ESC) {
 			backlight_pwm = old_backlight_pwm;
 			set_backlight();
-			enc_group_close(slide);
+			transient_close(slide);
 			return;
 		}
 	default:
@@ -385,10 +393,7 @@ light_slide(edisp_page_t *epage)
 	lv_slider_set_range(slide, 10, 100);
 	lv_slider_set_value(slide, backlight_pwm, LV_ANIM_OFF);
 	lv_obj_set_event_cb(slide, light_action);
-	lv_obj_move_foreground(slide);
-	lv_obj_align(slide, lv_top_trs, LV_ALIGN_CENTER, 0, 0);
-	lv_group_set_editing(encg, false);
-	enc_group_focus(slide, true);
+	transient_open(slide);
 	old_backlight_pwm = backlight_pwm;
 	if (backlight_status == OFF) {
 		backlight_status = ON;
@@ -405,13 +410,13 @@ page_action(lv_obj_t *list, lv_event_t event)
 	case LV_EVENT_VALUE_CHANGED:
 		value = lv_ddlist_get_selected(list);
 		printf("new page %d\n", value);
-		enc_group_close(list);
+		transient_close(list);
 		switch_to_page(value);
 		break;
 	case LV_EVENT_KEY:
 		key = *((uint32_t *)lv_event_get_data());
 		if (key == LV_KEY_ESC) {
-			enc_group_close(list);
+			transient_close(list);
 		}
 		break;
 	}
@@ -435,9 +440,7 @@ page_list(void)
 	lv_ddlist_set_selected(list, current_page);
 	lv_obj_set_event_cb(list, page_action);
 	lv_ddlist_set_stay_open(list, true);
-	lv_obj_move_foreground(list);
-	lv_obj_align(list, lv_top_trs, LV_ALIGN_CENTER, 0, 0);
-	enc_group_focus(list, true);
+	transient_open(list);
 }
 
 static void
